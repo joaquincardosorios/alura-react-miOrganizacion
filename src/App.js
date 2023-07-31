aba562e7ac6637a36235eb01cf5db0887355f95a
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-31 16:48:33 -0400

    feat(header)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <img src="/img/header.png" alt="Org"/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
